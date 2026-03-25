2022fdac515eb15e76752bfbe73381853590f9f8
Sveta <artist.sv.chepurna@gmail.com>
2026-03-25 13:50:59 +0100

    Add a Page Title. Add container for Pokemon's Cards

--- src/script.js
+++ src/script.js
@@ -1,9 +1,26 @@
+// ........... POKEDEX Fetch
+const containerPokedex = document.getElementById("pokedex");
+
+if (containerPokedex) {
+    const fetchPokemon = async () => {
+        for (let i = 1; i <= 12; i++) {
+
+        }
+
+    }  
+    
+    fetchPokemon();
+}
+
+
+
+// ........... THE POKEMON SEARCH ..............
 const searchPokemon = document.getElementById("searchBtn");
 const nameIdInput = document.getElementById("name-id");
 
 
 if (searchPokemon && nameIdInput) {
-    searchPokemon.addEventListener("click", (event) => {
+    searchPokemon.addEventListener("click", async (event) => {
         event.preventDefault(); // keep wave from form behavior
         const query = nameIdInput?.value?.trim(); // query reads user input.
         // (?.) ensures that if nameIdInput is null or undefined returns undefined instead an error.
@@ -12,6 +29,7 @@ if (searchPokemon && nameIdInput) {
         // The .trim() method then removes any leading or trailing whitespace from that string
 
         if (!query) {
+            alert("Enter Pokemon name or id to search for!");
             searchPokemon.style.backgroundColor = "grey";
             setTimeout(() => {
             searchPokemon.style.backgroundColor = "white";
@@ -20,11 +38,26 @@ if (searchPokemon && nameIdInput) {
         }
 
         // search logic here (fetch, render, etc.)
+            // button color changes
         searchPokemon.style.backgroundColor = "green";
-        // immediate toggle style
+            // immediate toggle style
         setTimeout(() => {
             searchPokemon.style.backgroundColor = "white";
         }, 150);
+
+            
+        // Send a request by Pokemon ID or name to the server of PokeApi using Fetch API
+
+            // Extract the actual user input value
+        let searchInputData = document.getElementById("name-id").value;
+
+        const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchInputData}`);
+        
+        if (apiResponse.ok) {
+            const pokResponseData = await apiResponse.json();
+            console.log(pokResponseData);
+            nameIdInput.value = "";
+        }
 
     });
 }
