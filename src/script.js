e56d6898a6873d177ced1ba1d4b98d33d91b6b1d
Sveta <artist.sv.chepurna@gmail.com>
2026-03-25 16:11:53 +0100

    Pokemon information Fetch from Poke API for 30 pokemons - 1 page

--- src/script.js
+++ src/script.js
@@ -1,17 +1,46 @@
 // ........... POKEDEX Fetch Auto Generate ...................
-    // automatically generates 151 Pokemon cards on the Bench page
+    // automatically generates 30 (1 page for pagination) Pokemon cards on the Bench page
 const containerPokedex = document.getElementById("pokedex"); // div container where All Pokemon Cards will be located
 const autoGenerateBtn = document.getElementById("autogenBtn"); // "Auto Generate" button
 
-if (containerPokedex) {
-    const fetchPokemon = async () => {
-        for (let i = 1; i <= 12; i++) {
+if (containerPokedex && autoGenerateBtn ) {
+    autoGenerateBtn.addEventListener("click", async (event) => {
+        event.preventDefault(); // keep wave from form behavior
+        
+        // button color changes shows that button clicked
+        autoGenerateBtn.style.backgroundColor = "green";
+            // immediate toggle style
+            setTimeout(() => {
+                autoGenerateBtn.style.backgroundColor = "white";
+            }, 150);
 
+        // fetch loop for 30 Pokemons (1 page)
+        async function fetchAutoPokemon(num, page, index) {
+            let i = 1;
+            while (i <= num) {
+                pokemonIndex = index.toString();
+                const apiAutoPokResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`);
+
+                if (apiAutoPokResponse.ok) {
+                    const autoPokRespData = await apiAutoPokResponse.json();
+                    console.log("found:", autoPokRespData);
+                    index++;
+                    i++;
+                } else if (apiAutoPokResponse.status === 404) {
+                    index++;
+                    return; // There are some Pokemons that don’t exist but should, 999 doesn’t exist but 1000 does.
+                } else {
+                    console.error("API error", apiAutoPokResponse.status);
+                    alert(`Error: ${apiAutoPokResponse.status}`);
+                }
+            }
+            console.log(`Generated ${num} pokemons, on the ${page} page, and the last index is ${index}`);
         }
+        
 
-    }  
-    
-    fetchPokemon();
+        fetchAutoPokemon(30, 1, 1);
+
+    });  
 }
 
 
@@ -40,7 +69,7 @@ if (searchPokemonBtn && nameIdInput) {
         }
 
         // search logic here (fetch, render, etc.)
-            // button color changes
+            // button color changes shows that button clicked
         searchPokemonBtn.style.backgroundColor = "green";
             // immediate toggle style
         setTimeout(() => {
